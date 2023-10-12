165a270173fd9ee1b256fae48e33c68bc013f701
dostonwebdev <dostone320@gmail.com>
2023-10-12 23:55:33 +0500

    homwork

---
+++ main.js
@@ -0,0 +1,47 @@
+// for 1
+
+// let k = 5;
+// let n = 7;
+// for (i = 1; i <= n; i++) {
+//   console.log(k);
+// }
+
+// for 4
+
+// let kg = 7000;
+// for (i = 1; i <= 10; i ++) {
+//   console.log(i*kg);
+// }
+
+// for 5
+
+// let kg = 5000;
+// for (i = 0.1; i <= 1; i += 0.1) {
+//   console.log(i * kg);
+// }
+
+// for 6
+
+// let kg = 2000;
+// for (i = 1.2; i <= 2; i +=0.1) {
+//   console.log(i * kg);
+// }
+
+// for 7
+// let mul = 0;
+// let a = 5;
+// let b = 7;
+// for (i = 1; i <= b; i++) {
+//   mul += i;
+// }
+// console.log(mul);
+
+// for 8
+// let mul = 1;
+
+// let a = 5;
+// let b = 7;
+// for (i = 1; i <= b; i++) {
+//   mul *= i;
+// }
+// console.log(mul);
